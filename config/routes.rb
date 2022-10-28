668aa023c93dfb3fba1186433275b86a625feb66
kyao0 <kyao0@chicagobooth.edu>
2022-10-28 20:19:06 +0000

    D ynamic

--- config/routes.rb
+++ config/routes.rb
@@ -21,20 +21,22 @@ Rails.application.routes.draw do
   get("/zodiacs/sagittarius", { :controller => "fire", :action => "archer" })
   
   get("/zodiacs/taurus", { :controller => "earth", :action => "bull" })
-  # get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
-  # get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
+  get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
+  get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
   
-  # get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
-  # get(" /zodiacs/libra", { :controller => "air", :action => "scales" })
-  # get ("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
+  get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
+  get("/zodiacs/libra", { :controller => "air", :action => "scales" })
+  get("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
   
-  # get("/zodiacs/cancer " { :controller => "earth" :action => "crab" })
-  # get("/zodiac/ scorpio", { :controller => "Water", :action => "scorpion" })
-  # get("/zodiacs/pisces", { :controller => "water", :action => "fish " })
+  get("/zodiacs/cancer", { :controller => "water", :action => "crab" })
+  get("/zodiacs/scorpio", { :controller => "water", :action => "scorpion" })
+  get("/zodiacs/pisces", { :controller => "water", :action => "fish" })
+
 
   # PART 3: MORE R→C→A→V PRACTICE
   # ==========================
-  
+  get("/roll/:number_of_dice/:how_many_sides", { :controller => "dice", :action => "infinity_and_beyond" })
+
   # Let users visit the following URLs.
   # See the target to find out what the interface should be — then match it.
   # Follow the standard R→C→A→V procedure:
